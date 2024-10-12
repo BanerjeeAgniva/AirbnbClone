5a9f21724639db3e00238f847f4f8fd757240fd2
Agniva Banerjee <agniva.banerjee@harness.io>
2024-10-12 13:27:57 +0530

    check

--- App.js
+++ App.js
@@ -8,7 +8,10 @@ Check the Figma file for the design specifics.
 export default function App() {
     return (
         <div>
+            <h1> Agniva </h1>
             <Navbar />
+            <h1> Agniva </h1>
+            <h1> Agniva </h1>
         </div>
     )
 }
